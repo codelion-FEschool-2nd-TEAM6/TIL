c0cef997c13d097309869ea8df5be040db72216c
Jihoon Chae <93469760+humormint@users.noreply.github.com>
2022-04-28 00:02:18 +0900

    Add files via upload

---
+++ 20220426~20220501/이호준Javascript/002.js
@@ -0,0 +1,5 @@
+document.getElementById("one").innerHTML =
+"hello <strong>world document</strong>"; // html로 넣어줄거다
+console.log("hello world console");
+window.alert("hello world alert");
+document.write("hello!!"); // 요소 없는데 쓸 수 있음
